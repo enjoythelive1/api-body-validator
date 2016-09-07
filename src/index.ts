531eb95dec0e94758ad8222891903c8f72f72a91
Fausto Guerrero <enjoythelive1@gmail.com>
2016-09-07 18:08:28 -0400

    Exposed validations

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+/// <reference path="../typings/index.d.ts" />
 export * from './validation-error';
 export * from './validator';
+
+export * from './validations/validation';
+export * from './validations/string-validation';
+export * from './validations/required-validation';
+export * from './validations/reference-validator';
+export * from './validations/object-validator';
+export * from './validations/must-not-have-validation';
+export * from './validations/array-validation';
